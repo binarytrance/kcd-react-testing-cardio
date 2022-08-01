3346fe81e45b1e3e88a387d146287bfc62edc3d2
binarytrance <ganeshan.dash@gmail.com>
2022-08-01 14:29:51 +0530

    1st exercies

--- src/__tests__/exercise/01extra-1.js
+++ src/__tests__/exercise/01extra-1.js
@@ -11,15 +11,28 @@ import Counter from '../../components/counter'
 // Luckily, it's handled for you by React Testing Library :)
 global.IS_REACT_ACT_ENVIRONMENT = true
 
+beforeEach(() => {
+  document.body.innerHTML = ''
+})
+
 test('counter increments and decrements when the buttons are clicked', () => {
   // 🐨 create a div to render your component to (💰 document.createElement)
   const element = document.createElement('div')
+  const incrementMouseClick = new MouseEvent('click', {
+    bubbles: true, // React uses event delegation and bubbling is required for it to work
+    cancelable: true,
+    button: 0,
+  })
+  const decrementMouseClick = new MouseEvent('click', {
+    bubbles: true, // React uses event delegation and bubbling is required for it to work
+    cancelable: true,
+    button: 0,
+  })
   // 🐨 append the div to document.body (💰 document.body.append)
   document.body.append(element)
   // 🐨 use createRoot to render the <Counter /> to the div
   const root = createRoot(element)
   act(() => root.render(<Counter />))
-  console.log(document.body.innerHTML)
   // 🐨 get a reference to the increment and decrement buttons:
   //   💰 div.querySelectorAll('button')
   const [decrementButton, incrementButton] = element.querySelectorAll('button')
@@ -29,16 +42,18 @@ test('counter increments and decrements when the buttons are clicked', () => {
   // 🐨 expect the message.textContent toBe 'Current count: 0'
   expect(messageElement.textContent).toBe('Current count: 0')
   // 🐨 click the increment button (💰 act(() => increment.click()))
-  act(() => incrementButton.click())
+  //   act(() => incrementButton.click())
+  act(() => incrementButton.dispatchEvent(incrementMouseClick))
   // 🐨 assert the message.textContent
   expect(messageElement.textContent).toBe('Current count: 1')
   // 🐨 click the decrement button (💰 act(() => decrement.click()))
-  act(() => decrementButton.click())
+  //   act(() => decrementButton.click())
+  act(() => decrementButton.dispatchEvent(decrementMouseClick))
   // 🐨 assert the message.textContent
   expect(messageElement.textContent).toBe('Current count: 0')
   //
   // 🐨 cleanup by removing the div from the page (💰 div.remove())
-  element.remove() // if an earlier test fails, remove won't run and the next tests will fail. hence we use beforeEach and reset our DOM. (see 01extra.js)
+  //   element.remove()
   // 🦉 If you don't cleanup, then it could impact other tests and/or cause a memory leak
 })
 
